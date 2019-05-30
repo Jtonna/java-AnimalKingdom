e92cb0d7dd57effbcb7ead42c62216449bdffec9
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-30 13:27:46 -0700

    Project file setup

---
+++ animalkingdom/src/animalkingdom/AnimalActions.java
@@ -0,0 +1,5 @@
+package animalkingdom;
+
+public abstract class AnimalActions {
+	
+}
---
+++ animalkingdom/src/animalkingdom/AnimalClass.java
@@ -0,0 +1,13 @@
+package animalkingdom;
+
+public class Mamals extends AnimalActions {
+
+}
+
+public class Birds extends AnimalActions {
+
+}
+
+public class Fish extends AnimalActions {
+
+}
