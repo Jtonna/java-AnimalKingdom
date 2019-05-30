544aa2b275c56b2f8339f912471e32a9c5c5e6a4
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-30 14:04:08 -0700

    Working on more of the setup and commenting on existing code.

--- animalkingdom/src/animalkingdom/AnimalActions.java
+++ animalkingdom/src/animalkingdom/AnimalActions.java
@@ -1,5 +1,10 @@
 package animalkingdom;
 
 public abstract class AnimalActions {
-	
+
+	public abstract String getName();
+	public abstract String breath();
+	public abstract String walk();
+	public abstract int getYearDiscovered();
+
 }
--- animalkingdom/src/animalkingdom/Main.java
+++ animalkingdom/src/animalkingdom/Main.java
@@ -1,7 +1,11 @@
 package animalkingdom;
 
+// every java app must have public 'Main' class.
 public class Main
 {
+	// below is the public 'main' method. This is considered an entry point. It's also a child of the public class Main.
+	// this has to be 'public' because java looks for the public main method signature. 
+	// warning: if this is private the app will compile but not run as the main program's main entry point
 	public static void main(String[] args)
 	{
 		System.out.println("Hello There");
